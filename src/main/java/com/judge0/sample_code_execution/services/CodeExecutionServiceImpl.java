40649d11896c95a515c597f1bcad4b86002cbe81
Nikola Bulat <nbulat99@gmail.com>
2021-09-02 14:09:11 +0200

    Replace WebClient with RestTemplate

--- src/main/java/com/judge0/sample_code_execution/services/CodeExecutionServiceImpl.java
+++ src/main/java/com/judge0/sample_code_execution/services/CodeExecutionServiceImpl.java
@@ -1,30 +1,24 @@
 package com.judge0.sample_code_execution.services;
 
 import com.judge0.sample_code_execution.model.Response;
-import com.judge0.sample_code_execution.model.StatusToken;
 import com.judge0.sample_code_execution.model.SubmissionParameter;
-import exception_handling.ResponseException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
-import reactor.core.publisher.Mono;
-
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CodeExecutionServiceImpl implements CodeExecutionService{
 
-    private final WebClient webClient;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public CodeExecutionServiceImpl(WebClient webClient) {
-        this.webClient = webClient;
+    public CodeExecutionServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     @Override
     public Response submitCode(SubmissionParameter submissionParameter)  {
-        StatusToken statusToken = getStatusToken(submissionParameter);
+//        StatusToken statusToken = getStatusToken(submissionParameter);
         //I will implement the logic of making a request multiple times until the code execution is done. This is just temporary.
 //        try {
 //            Thread.sleep(3000);
@@ -37,24 +31,26 @@ public class CodeExecutionServiceImpl implements CodeExecutionService{
 //                .retrieve()
 //                .bodyToMono(Response.class)
 //                .block();
-        return webClient.post()
-                .uri("/submissions/?base64_encoded=false&wait=true")
-                .body(Mono.just(submissionParameter), Response.class)
-                .retrieve()
-                .bodyToMono(Response.class)
-                .block();
+
+//        return webClient.post()
+//                .uri("/submissions/?base64_encoded=false&wait=true")
+//                .body(Mono.just(submissionParameter), Response.class)
+//                .retrieve()
+//                .bodyToMono(Response.class)
+//                .block();
+        return restTemplate.postForObject("https://ce.judge0.com/submissions/?base64_encoded=false&wait=true", submissionParameter, Response.class);
     }
 
-    private StatusToken getStatusToken(SubmissionParameter submissionParameter){
-        try {
-            return webClient.post()
-                    .uri("/submissions/?base64_encoded=false&wait=false")
-                    .body(Mono.just(submissionParameter), StatusToken.class)
-                    .retrieve()
-                    .bodyToMono(StatusToken.class)
-                    .block();
-        } catch (WebClientResponseException e) {
-            throw new ResponseException(e.getMessage());
-        }
-    }
+//    private StatusToken getStatusToken(SubmissionParameter submissionParameter){
+//        try {
+//            return webClient.post()
+//                    .uri("/submissions/?base64_encoded=false&wait=false")
+//                    .body(Mono.just(submissionParameter), StatusToken.class)
+//                    .retrieve()
+//                    .bodyToMono(StatusToken.class)
+//                    .block();
+//        } catch (WebClientResponseException e) {
+//            throw new ResponseException(e.getMessage());
+//        }
+//    }
 }
